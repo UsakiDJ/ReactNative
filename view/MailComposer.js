eef96907286d691f0022ed6b858048d24547af80
DRIDI Jessim <usakijess@gmail.com>
2023-04-19 17:24:06 +0200

    Ajout du système d'envoi de mail

--- view/MailComposer.js
+++ view/MailComposer.js
@@ -2,12 +2,12 @@ import * as MailComposer from 'expo-mail-composer';
 
 export const Mail = async (file) => {
 
-    console.log("sauce")
+    /// Mise en place des options pour préparer l'envoi du mail
     const MailComp = {
         attachments: [file]
     }
 
-
+    //Ouverture de l'application Mail avec les options configurés plus haut. 
     await MailComposer.composeAsync(MailComp)
 
 };
